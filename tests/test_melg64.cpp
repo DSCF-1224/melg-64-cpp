6337590c01ef42478d11c3fe8e14724dae72231f
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-05-06 23:59:08 +0900

    test: add `tests/test_melg64.cpp`

---
+++ tests/test_melg64.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+// https://cppreference.com/cpp/header/cstdlib
+// https://cppreference.com/cpp/utility/program/EXIT_status
+// https://cpprefjp.github.io/reference/cstdlib.html
+// https://cpprefjp.github.io/reference/cstdlib/exit_success.html
+
+#include <random>
+// https://cppreference.com/cpp/header/random
+// https://cppreference.com/cpp/numeric/random/uniform_random_bit_generator
+// https://cpprefjp.github.io/reference/random.html
+// https://cpprefjp.github.io/reference/random/uniform_random_bit_generator.html
+
+#include <melg64/melg64.hpp>
+
+int main(void) { return EXIT_SUCCESS; }
