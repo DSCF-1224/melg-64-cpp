8f8976d6e457f0a60490aada3fada8cce1f96ec3
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-05-08 07:00:31 +0900

    test: add output of `BUILD_CXX_FLAGS` to `stdout`

--- tests/test_melg64.cpp
+++ tests/test_melg64.cpp
@@ -27,6 +27,7 @@ void PrintBuildInfo() {
   std::cout << "Compiler Version : " << BUILD_COMPILER_VERSION << std::endl;
   std::cout << "std              : C++" << BUILD_CXX_STANDARD << std::endl;
   std::cout << "Build Type       : " << BUILD_BUILD_TYPE << std::endl;
+  std::cout << "CXX Flags        : " << BUILD_CXX_FLAGS << std::endl;
 }
 
 int main(void) {
