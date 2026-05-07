4c396915f52829d67810bd8f07fddb627e6fd1ff
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-05-07 23:22:30 +0900

    test: add `#include <iostream>`

--- tests/test_melg64.cpp
+++ tests/test_melg64.cpp
@@ -4,6 +4,14 @@
 // https://cpprefjp.github.io/reference/cstdlib.html
 // https://cpprefjp.github.io/reference/cstdlib/exit_success.html
 
+#include <iostream>
+// https://cppreference.com/cpp/header/iostream
+// https://cppreference.com/cpp/io/cout
+// https://cppreference.com/cpp/io/manip/endl
+// https://cpprefjp.github.io/reference/iostream.html
+// https://cpprefjp.github.io/reference/iostream/cout.html
+// https://cpprefjp.github.io/reference/ostream/endl.html
+
 #include <random>
 // https://cppreference.com/cpp/header/random
 // https://cppreference.com/cpp/numeric/random/uniform_random_bit_generator
