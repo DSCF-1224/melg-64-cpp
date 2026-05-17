0693d1099a340434f6c9873bfd3c89d7183a8a70
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-05-18 08:40:55 +0900

    Squashed commit of the following:
    commit 1e081ac5de9dcd942a576250938f8215f6f9de67
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sun May 17 07:45:17 2026 +0900
    
        test: add test_known_output_melg607
        Currently fails to compile due to bugs in `melg_base` member function pointer usage.
    
    commit 2564cf8252a9b388e8b91f992e314ecd3042986f
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sun May 17 07:31:40 2026 +0900
    
        test: add `#include <span>`
    
    commit c21c8873584475d08c37b4944e591ece1f20fa04
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sun May 17 07:29:56 2026 +0900
    
        chore: reorder includes in test_melg64.cpp
    
    commit 7b2b220fc4ca771776f9695f88b98b67ea1a98b9
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sun May 17 07:22:22 2026 +0900
    
        test: add shared `init_key_a` for array seed tests
    
    commit a3185ad6aef68f4343676dd4e789a5edc8a30594
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sun May 17 07:16:24 2026 +0900
    
        test: add `#include <array>`

--- tests/test_melg64.cpp
+++ tests/test_melg64.cpp
@@ -1,3 +1,7 @@
+#include <array>
+// https://cppreference.com/cpp/header/array
+// https://cpprefjp.github.io/reference/array.html
+
 #include <cstdlib>
 // https://cppreference.com/cpp/header/cstdlib
 // https://cppreference.com/cpp/utility/program/EXIT_status
@@ -18,12 +22,16 @@
 // https://cpprefjp.github.io/reference/random.html
 // https://cpprefjp.github.io/reference/random/uniform_random_bit_generator.html
 
-#include <melg64/melg64.hpp>
-// target of this test
+#include <span>
+// https://cppreference.com/cpp/header/span
+// https://cpprefjp.github.io/reference/span.html
 
 #include <string_view>
 // https://cppreference.com/cpp/header/string_view
 // https://cpprefjp.github.io/reference/string_view.html
+
+#include <melg64/melg64.hpp>
+// target of this test
 
 #include "build_info.hpp"
 
@@ -53,6 +61,25 @@ void PrintBuildInfo() {
   }
 
   std::cout << std::endl << std::endl;
+}
+
+const std::array<melg64::result_type, 4> init_key_a = {0x12345UL, 0x23456UL,
+                                                       0x34567UL, 0x45678UL};
+
+bool test_known_output_melg607(std::span<const melg64::result_type> init_key) {
+  static constexpr melg64::result_type expected[10] = {
+      12495950309458289112UL, 8163910988915845065UL,  17447112683145787935UL,
+      14392119910362097645UL, 7164909824801924305UL,  17038754296801418064UL,
+      10871240116890307231UL, 12692713980656253045UL, 10435959733805108698UL,
+      5542897018756383954UL};
+
+  melg64::melg607 engine(init_key);
+
+  for (auto e : expected) {
+    if (engine() != e) return false;
+  }
+
+  return true;
 }
 
 int main(void) {
