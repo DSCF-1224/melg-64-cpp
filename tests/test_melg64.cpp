cd99d6de72e0f9662378afbfd9594d904386c65c
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-05-19 06:59:00 +0900

    test: register `seed_reset_melg4253` in test runner

--- tests/test_melg64.cpp
+++ tests/test_melg64.cpp
@@ -287,7 +287,8 @@ int main(void) {
        []() { return test_default_constructor<melg64::melg44497>(); }},
       {"seed_reset_melg607", test_seed_reset<melg64::melg607>},
       {"seed_reset_melg1279", test_seed_reset<melg64::melg1279>},
-      {"seed_reset_melg2281", test_seed_reset<melg64::melg2281>}};
+      {"seed_reset_melg2281", test_seed_reset<melg64::melg2281>},
+      {"seed_reset_melg4253", test_seed_reset<melg64::melg4253>}};
 
   int count_failed = 0;
 
