509441b8cc09be760677fc004873ef72920947d6
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-05-20 06:31:18 +0900

    refactor: simplify `test_default_constructor` registration for `melg2281`

--- tests/test_melg64.cpp
+++ tests/test_melg64.cpp
@@ -276,7 +276,7 @@ int main(void) {
       {"default_constructor_melg1279",
        test_default_constructor<melg64::melg1279>},
       {"default_constructor_melg2281",
-       []() { return test_default_constructor<melg64::melg2281>(); }},
+       test_default_constructor<melg64::melg2281>},
       {"default_constructor_melg4253",
        []() { return test_default_constructor<melg64::melg4253>(); }},
       {"default_constructor_melg11213",
